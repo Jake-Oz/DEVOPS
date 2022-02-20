94dd14ebf814675fa9e99c59a7a196200d259f6a
Jake Campbell <jakeandrach@me.com>
2022-02-20 21:41:06 +1100

    added hamburger menu pop out

--- htmlPages/app/js/script.js
+++ htmlPages/app/js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello World!!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function(){
+    if (header.classList.contains('open')){ // close hamburger menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        });
+    } else {
+        body.classList.add('noscroll');
+        header.classList.add('open'); //open hamburger menu
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
+    }
+});
